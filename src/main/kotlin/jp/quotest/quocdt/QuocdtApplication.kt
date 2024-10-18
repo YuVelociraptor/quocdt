fc1245d376f78f824c18463ea40f961b1dfc23bc
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2024-10-18 13:00:09 +0900

    initial commit

---
+++ src/main/kotlin/jp/quotest/quocdt/QuocdtApplication.kt
@@ -0,0 +1,11 @@
+package jp.quotest.quocdt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuocdtApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuocdtApplication>(*args)
+}
---
+++ src/test/kotlin/jp/quotest/quocdt/QuocdtApplicationTests.kt
@@ -0,0 +1,13 @@
+package jp.quotest.quocdt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuocdtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
