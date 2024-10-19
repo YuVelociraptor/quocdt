0446494c2a1112d022cf48340b41fd63f0edc88b
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2024-10-19 16:15:19 +0900

    テーブル定義

--- src/main/resources/db/migration/V1__initial_setup.sql
+++ src/main/resources/db/migration/V1__initial_setup.sql
@@ -1,4 +1,21 @@
+create table books  (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    price DECIMAL(10, 0) NOT NULL,
+    publication_status boolean NOT NULL
+);
+
 create table authors  (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
+    name VARCHAR(100) NOT NULL,
+    birth_date DATE NOT NULL
+);
+
+create table book_authors  (
+    id SERIAL PRIMARY KEY,
+    book_id INT NOT NULL,
+    author_id INT NOT NULL,
+    UNIQUE (book_id, author_id),
+    FOREIGN KEY (book_id) REFERENCES books(id),
+    FOREIGN KEY (author_id) REFERENCES authors(id)
 );
