bfb3e6cf1d1525834c49ce295b006dba06538130
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2024-10-28 04:51:36 +0900

    rename to AuthorController

--- src/main/kotlin/jp/quotest/quocdt/controller/AuthorController.kt
+++ src/main/kotlin/jp/quotest/quocdt/controller/AuthorController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
-class Controller {
+class AuthorController {
 
     @GetMapping
     fun getTest(): String{
